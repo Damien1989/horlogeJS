dce21f69b297aa3fd9059d194cce3006400b5a7a
damien <contact.damiendiaz@gmail.com>
2024-05-08 22:03:52 +0200

    html and css finished

---
+++ app.js
@@ -0,0 +1,7 @@
+const secondsHand = document.getElementById("seconds-hands");
+const minutesHand = document.getElementById("minutes-hands");
+const hoursHands = document.getElementById("hours-hands");
+
+function getTime {
+    
+}
