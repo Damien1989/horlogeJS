df070dab7ff60512f5809238275031cb6ad3c3b3
damien <contact.damiendiaz@gmail.com>
2024-05-08 23:19:17 +0200

    js clock

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const secondsHand = document.getElementById("seconds-hands");
-const minutesHand = document.getElementById("minutes-hands");
-const hoursHands = document.getElementById("hours-hands");
+const secondsHand = document.getElementById("seconds-hands")
+const minutesHand = document.getElementById("minutes-hands")
+const hoursHands = document.getElementById("hours-hands")
 
-function getTime {
-    
+function getTime() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const timeInterval = 6
+
+    secondsHand.style.transform = "rotate(" + (seconds * timeInterval) +"deg)"
+    minutesHand.style.transform = "rotate(" + (minutes * timeInterval + seconds/10) +"deg)"
+    hoursHands.style.transform = "rotate(" + (hours * 30 + minutes/2) +"deg)"
 }
+
+setInterval(getTime, 100)
